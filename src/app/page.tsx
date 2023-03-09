0f0eb514962275877f1748cb456fb6ffffba05f4
吕不为 <yblunan@gmail.com>
2023-03-09 16:47:10 +0800

    nice

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 'use client';
-import { useCallback, useEffect, useLayoutEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
 import styles from './page.module.css';
 import ReactMarkdown from 'react-markdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { darcula } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import Image from 'next/image';
+import dayjs from 'dayjs';
+
 // darcula.;
 function CodeBlock(p: any) {
   let lang = p.className ? p.className.replace('language-', '') : '';
@@ -42,7 +43,7 @@ async function generateResponse(messages: Array<Msg>) {
 
 }
 
-let id = 0;
+let idBase = 0;
 type Msg = { id: string | number; time: number; role: 'user' | 'system'; loading?: boolean; err?: boolean; content: string };
 export default function Home() {
   const [loading, setLoading] = useState(true);
@@ -53,11 +54,16 @@ export default function Home() {
     const history = localStorage.getItem('history');
     if (history) {
       msgRef.current = JSON.parse(history);
+      if (msgRef.current.length > 0) {
+        idBase = msgRef.current[msgRef.current.length - 1].id as number;
+      } else {
+        msgRef.current = [];
+      }
     }
     // setInput('');
     setLoading(false);
     setTimeout(() => {
-      listRef.current?.scrollTo({ top: listRef.current?.scrollHeight });
+      document.body.scrollTo({ top: listRef.current?.scrollHeight });
       inputRef.current?.focus();
     }, 800);
   }, []);
@@ -68,13 +74,13 @@ export default function Home() {
       return;
     }
 
-    id = id + 1;
+    idBase = idBase + 1;
     let src = {
       time: Date.now(),
       role: 'user',
       content: ipt,
       err: false,
-      id,
+      id: idBase,
       loading: true,
     };
     // alert(srcs);
@@ -82,13 +88,13 @@ export default function Home() {
     inputRef.current!.value = '';
     setLoading(true);
     setTimeout(() => {
-      listRef.current?.scrollTo({ top: listRef.current?.scrollHeight });
+      document.body.scrollTo({ top: listRef.current?.scrollHeight });
     }, 10);
     try {
       const res = await generateResponse(msgRef.current);
       msgRef.current.push({
         time: Date.now(),
-        id,
+        id: idBase,
         role: 'system',
         content: res.map((item: any) => item.text).join(''),
       });
@@ -97,60 +103,89 @@ export default function Home() {
         msgRef.current = msgRef.current.slice(-100);
       }
       localStorage.setItem('history', JSON.stringify(msgRef.current.map(({ id, time, role, content }) => ({ id, time, role, content }))));
-  } catch (e) {
-    src.err = true;
-    console.error(e);
+    } catch (e) {
+      src.err = true;
+      console.error(e);
+    }
+    src.loading = false;
+    setLoading(false);
+    setTimeout(() => {
+      document.body.scrollTo({ top: listRef.current?.scrollHeight });
+      inputRef.current?.focus();
+    }, 10);
+  };
+
+  const reSend = (id: string | number) => {
+    const msg = msgRef.current.find((item) => item?.id === id);
+    if (!msg) {
+      return;
+    }
+
+    delete msgRef.current[msgRef.current.indexOf(msg)];
+    const m = msg.content;
+    inputRef.current!.value = m;
+    // setTimeout(() => {
+    // trigger
+    setLoading(false);
+    // sendMessage();
+    // }, 500);
   }
-  src.loading = false;
-  setLoading(false);
-  setTimeout(() => {
-    listRef.current?.scrollTo({ top: listRef.current?.scrollHeight });
-    inputRef.current?.focus();
-  }, 10);
-};
+  const onKeyUP = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.keyCode === 13 && !e.shiftKey) {
+      sendMessage();
+    }
+  };
 
-const reSend = (id: string | number) => {
-  const msg = msgRef.current.find((item) => item?.id === id);
-  if (!msg) {
-    return;
-  }
+  useEffect(() => {
+    const resize = () => {
+      document.body.scrollTo({ top: listRef.current?.scrollHeight });
+    };
+    window.addEventListener('resize', resize);
+    return () => {
+      window.removeEventListener('resize', resize);
+    };
+  }, []);
 
-  delete msgRef.current[msgRef.current.indexOf(msg)];
-  const m = msg.content;
-  inputRef.current!.value = m;
-  // setTimeout(() => {
-  // trigger
-  sendMessage();
-  // }, 500);
-}
-const onKeyUP = (e: React.KeyboardEvent<HTMLInputElement>) => {
-  if (e.keyCode === 13 && !e.shiftKey) {
-    sendMessage();
-  }
-};
-
-
-return (
-  <div className={styles.main}>
-    <div ref={listRef} className={styles.msg}>
-      {msgRef.current.map((message) => (
-        <>
-          <div key={message.id} className={(message.role == 'user' ? styles.me : '') + (message.err ? ' ' + styles.err : '')}>
-            {message.role == 'system' ? <ReactMarkdown components={{
-              code: CodeBlock,
-            }}>{message.content}</ReactMarkdown> : <>
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              {message.err ? <img onClick={() => reSend(message.id)} className={styles.r} src="/r.svg" alt={''} /> : ''}
-              {message.content}
-            </>}
-          </div>
-          {message.loading ? <div className={styles.loading}>...</div> : ''}
-        </>))}
-    </div>
-    <div className={styles.input}>
-      <input className={styles.ipt} ref={inputRef} autoFocus onKeyUpCapture={onKeyUP} disabled={loading} />
-      <button disabled={loading} onClick={sendMessage as any}>Send</button>
-    </div>
-  </div>
-);
+  return (
+    <div className={styles.main}>
+      <div ref={listRef} className={styles.msg}>
+        {msgRef.current.map(({ time, role, content, id, loading, err }) => {
+          const c = role == 'user' ? styles.user : styles.system;
+          return (
+            <div key={id} className={err ? `${c} ${styles.err}` : c}>
+              <div className={styles.t}>
+                <img src={role == 'system' ? "/system.svg" : "/user.svg"} />
+                <span>
+                  {role == 'system' ? '机器人' : '我'}
+                </span>
+                <i>{dayjs(time).format('HH:mm:ss')}</i>
+              </div>
+              <div className={styles.c}>
+                <p>
+                  {role == 'system' ?
+                    <ReactMarkdown components={{
+                      code: CodeBlock,
+                    }}>{content}</ReactMarkdown> :
+                    <>
+                      {/* eslint-disable-next-line @next/next/no-img-element */}
+                      {err ? <img onClick={() => reSend(id)} className={styles.r} src="/r.svg" alt={''} /> : ''}
+                      {content}
+                    </>}
+                </p>
+              </div>
+            </div>);
+        }
+        )}
+        {loading ? <div>
+          <div className={styles.t}><span>机器人:</span></div>
+          <div className={styles.s}>...</div></div> : null}
+      </div>
+      <div className={styles.bar}>
+        <div className={styles.input}>
+          <input className={styles.ipt} ref={inputRef} autoFocus onKeyUpCapture={onKeyUP} disabled={loading} />
+          <button disabled={loading} onClick={sendMessage as any}>Send</button>
+        </div>
+      </div>
+    </div >
+  );
 }
